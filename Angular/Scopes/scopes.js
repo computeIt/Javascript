a5058977756201c62f350afa2f03df2a88f2bb18
computeIt <pushok@mail.ru>
2017-07-25 11:23:55 +0300

    Create scopes.js

---
+++ Angular/Scopes/scopes.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Андрей on 24.07.2017.
+ */
+angular.module("scopes", [])
+    .controller("scopesCtrl", function($scope){
+        $scope.model = {};
+    })
+    .controller("first", function(){})
+    .controller("second", function(){})
